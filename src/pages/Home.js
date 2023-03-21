5b48231f1e78af0878e7a56738e2e62cf451c0f0
Jordan <jordansampson23@gmail.com>
2023-03-21 14:26:32 -0500

    Styling Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Cat from '../assets/images/logo.png';
+import NeonButton from '../components/NeonButton';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex home'>
+        <div className='overlay'>
+            <h1 className='gradient-text' >Where Furball Meets Fashion</h1>
+            <NeonButton text="Shop Now"/>
+        </div>
+        <div>
+            <img src={Cat} width='600px' alt="Cool Cat" />
+        </div>
+    </div>
   )
 }
 
