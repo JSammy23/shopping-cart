f15a623731e6ba313a495848e5c993552bee6b71
Jordan <jordansampson23@gmail.com>
2023-03-21 13:32:25 -0500

    Added Shop route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
+import Home from '../src/pages/Home';
+import Shop from "./pages/Shop";
 
 function App() {
   return (
     <>
       <Header title='Psuedo Shop' />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Routes>
     </>
   );
