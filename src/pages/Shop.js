1dc140b41cf777d5742e401647ca6bdba774a91c
Jordan <jordansampson23@gmail.com>
2023-03-22 09:56:52 -0500

    Import inventory

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,13 +2,21 @@ import React from 'react'
 import Card from '../components/Card';
 import Young from '../assets/images/youngPuss.avif';
 import Sweater from '../assets/images/sweater.avif';
+import Shades from '../assets/images/shades.avif';
+import Wizard from '../assets/images/wizard.avif';
+import Gary from '../assets/images/gary.webp';
+import Gown from '../assets/images/tie&gown.jpg';
 
 
 const Shop = () => {
 
     const inventory = [
         { title: 'Young Puss', image: Young, price: 24.99 },
-        { title: 'Sweater Weather', image: Sweater, price: 30 }
+        { title: 'Sweater Weather', image: Sweater, price: 30 },
+        { title: 'Shades', image: Shades, price: 14.99 },
+        { title: "You're a Wizard Fluffy!", image: Wizard, price: 22.99 },
+        { title: 'The Gary', image: Gary, price: 19.99 },
+        { title: 'The Graduate', image: Gown, price: 24.99 }
     ]
 
   return (
