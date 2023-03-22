84d042d39883af4dd5693bb33af0b548554b0c53
Jordan <jordansampson23@gmail.com>
2023-03-22 11:43:59 -0500

    Styled cart close button

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -11,8 +11,10 @@ const Cart = ({ isOpen, handleCloseCart}) => {
 
   return (
     <div className={cartClass}>
-        <h2>Your Shopping Cart</h2>
-        <button onClick={handleCartClose}>X</button>
+        <div className="cartHeader">
+            <h2>Your Shopping Cart</h2>
+            <button className='pulse' onClick={handleCartClose}>Close</button>
+        </div>
         {/* cart items listed here */}
     </div>
   )
